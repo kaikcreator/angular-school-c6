bbdd51daa3755863b6d9c358226e7d65f4afffec
-KaiK- <kaikcreator@users.noreply.github.com>
2019-06-25 16:18:17 +0200

    working in point 8

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 import {of, Observable} from 'rxjs';
 import {delay, tap} from 'rxjs/operators';
 import { User } from './user.interface';
+const md5 = require('md5'); 
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +14,14 @@ export class AuthService {
   public user:User = null;
   public redirectUrl:string = null;
 
-  constructor() {
+  constructor(private http:HttpClient) {
     this.user = JSON.parse(localStorage.getItem("user"));
    }
 
   login(email, password):Observable<User>{
-    return of({email:email, password:password, token:"123456"}).pipe(
-      delay(750),
+    const passwordHash = md5(password);
+    return this.http.post<User>(environment.apiUrl + '/signup', {email, password:passwordHash})
+    .pipe(
       tap(user => this.user = user),
       tap(user => localStorage.setItem("user", JSON.stringify(user)))
     );

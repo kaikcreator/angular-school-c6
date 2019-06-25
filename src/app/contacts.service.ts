b72b2113992cc5b4f5a254487598db3a1f3f6965
-KaiK- <kaikcreator@users.noreply.github.com>
2019-06-25 11:42:56 +0200

    completed point 7

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -3,30 +3,33 @@ import { Contact, PhoneType} from './contact.model';
 import { of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { mergeMap } from 'rxjs/operators';
+import { environment } from '../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ContactsService {
+    private resourceUrl = environment.apiUrl + '/contacts';
+    
     constructor(private http:HttpClient) { 
     }
 
     getContacts(){
-        return this.http.get<Contact[]>('http://localhost:3000/contacts');
+        return this.http.get<Contact[]>(this.resourceUrl);
     }
 
     getContactById(id){
-        return this.http.get<Contact>(`http://localhost:3000/contacts/${id}`);
+        return this.http.get<Contact>(`${this.resourceUrl}/${id}`);
     }
 
     public addContact(contact:Contact){
-        return this.http.post('http://localhost:3000/contacts', contact).pipe(
+        return this.http.post(this.resourceUrl, contact).pipe(
             mergeMap( (res:Contact) => this.updateContactImage(res, contact.pictureFile) )
         );
     }
 
     public updateContact(contact:Contact){
-        return this.http.patch(`http://localhost:3000/contacts/${contact.id}`, contact).pipe( mergeMap( (res:Contact) => this.updateContactImage(res, contact.pictureFile) ));
+        return this.http.patch(`${this.resourceUrl}/${contact.id}`, contact).pipe( mergeMap( (res:Contact) => this.updateContactImage(res, contact.pictureFile) ));
     }
 
     private updateContactImage(contact:Contact, file:File){
@@ -35,11 +38,11 @@ export class ContactsService {
         }
         let formData = new FormData();
         formData.append('picture', file, file.name);
-        return this.http.patch(`http://localhost:3000/contacts/${contact.id}`, formData);
+        return this.http.patch(`${this.resourceUrl}/${contact.id}`, formData);
     }
 
     public removeContact(contact:Contact){
-        return this.http.delete(`http://localhost:3000/contacts/${contact.id}`);
+        return this.http.delete(`${this.resourceUrl}/${contact.id}`);
     }
 
 }

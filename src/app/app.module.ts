dbe4ff7d749e1355991689d42a9d7af26e0deb07
-KaiK- <kaikcreator@users.noreply.github.com>
2019-06-25 14:51:12 +0200

    updated login view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
@@ -29,6 +30,7 @@ import { AppRoutingModule } from './app-routing.module';
     HttpClientModule,
     ContactDetailModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
